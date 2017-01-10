9901b71b75a2922739ce1f5b8a6681dc39095408
YangFan Ou <ouyangfan1991@gmail.com>
2017-01-10 14:18:08 +0800

    add compile support

--- example/src/main/java/com/asher/debug/example/MainActivity.java
+++ example/src/main/java/com/asher/debug/example/MainActivity.java
@@ -1,7 +1,9 @@
-package com.asher.debug;
+package com.asher.debug.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.asher.debug.R;
 
 public class MainActivity extends AppCompatActivity {
 
